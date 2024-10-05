92ccbd05ccbb6b7e4b6582566e00270b92275f00
æLtorio <aeltorio@universidadatlantico.org>
2024-10-05 09:56:49 +0200

    publishing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,12 +2,23 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { wasm } from '@rollup/plugin-wasm';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-    input: 'src/index.ts',
-    output: {
-        dir: 'dist',
-        format: 'cjs'
+export default [
+    {
+        input: 'src/index.ts',
+        output: {
+            dir: 'dist',
+            format: 'cjs'
+        },
+        plugins: [nodeResolve({ browser: true }), commonjs(), wasm(), typescript({ target: "es5", downlevelIteration: true })]
     },
-    plugins: [nodeResolve({ browser: true }), commonjs(), wasm(), typescript({ target: "es5", downlevelIteration: true })]
-};
+    {
+        input: 'src/index.ts',
+        output: {
+            file: 'dist/index.d.ts',
+            format: 'es'
+        },
+        plugins: [dts()]
+    }
+];
