e30b283b7c50aa3b80bb2f4aef47809c84dce3f1
æLtorio <aeltorio@universidadatlantico.org>
2024-10-07 12:38:01 +0200

    Update package.json version to 1.3.2 and include Buffer for browsers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { SentencePieceProcessor, cleanText } from "./sentencePieceProcessor";
 import { llama_3_1_tokeniser_b64 } from "./llama_3_1_tokeniser_model";
 import { smart_b64 } from "./smart";
 import { clean_30k_b64 } from "./clean_30k";
+import { Buffer as _Buffer } from "buffer";
 
 export { SentencePieceProcessor, cleanText, llama_3_1_tokeniser_b64, clean_30k_b64, smart_b64 };
-export default { SentencePieceProcessor, cleanText };
+export default { SentencePieceProcessor, cleanText };``
+
+if (typeof globalThis.Buffer === 'undefined') {
+  globalThis.Buffer = _Buffer;
+}
